5de5d3884e7be571930f201d03e3226223d2800f
glynk <glyn_knight@hotmail.com>
2022-05-20 10:15:05 +0100

    Get by ID working

--- router/recipes.js
+++ router/recipes.js
@@ -9,4 +9,17 @@ recipesRouter.get("/", function (req, res) {
     res.json(responseObject)
   });
 
+  recipesRouter.get("/:id", function (req, res) {
+    const recipeId = req.params.id;
+    let foundRecipe = {}
+    for ( let i = 0; i < recipes.length; i++){
+      if (Number(recipeId) === recipes[i].id){
+        foundRecipe = recipes[i];
+        break;
+      }
+    }
+    const responseObject = {success: true, data: foundRecipe}
+    res.json(responseObject)
+  });  
+
 export default recipesRouter;
