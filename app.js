636ebbe6c30f956cc35602857f6478fe33019b0e
JojokCreator <joeyk2k@hotmail.com>
2022-05-20 09:56:58 +0100

    first get working

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express";
-
 import { html } from "./config.js";
+import recipesRouter from "./router/recipes.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
+
+app.use("/recipes", recipesRouter)
 
 /* DO NOT CHANGE THIS ROUTE - it serves our front-end */
 app.get("/", function (req, res) {
@@ -16,3 +18,21 @@ app.get("/", function (req, res) {
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+//import recipes to router.js -done
+// router to app.js - done
+//Plan write get-all 
+//test the get-all
+// app.use in app js
+//Plan write get-by-id 
+//test the get-by-id
+// app.use in app js
+//Plan write post (new) 
+//test the post (new)
+// app.use in app js
+//Plan write put (change by id) 
+//test the put (new)
+// app.use in app js
+//Plan delete (delete by id) 
+//test the put (new)
+// app.use in app js
--- router/recipes.js
+++ router/recipes.js
@@ -1,3 +1,12 @@
 //Write your router code here!
+import recipes from "../libs/recipes.js";
+import express from "express";
 
-module.exports = recipesRouter;
+const recipesRouter = express.Router();
+
+recipesRouter.get("/", function (req, res) {
+    const responseObject = {success: true, data: recipes}
+    res.json(responseObject)
+  });
+
+export default recipesRouter;
